ee596a7f4c4d9778129c92bda79839c646b6639a
Philipp Prus <philipp@prcptv.ai>
2020-07-30 00:07:05 +0300

    added function for return error from all type form formik

--- src/index.js
+++ src/index.js
@@ -1,45 +1,75 @@
-import { Component } from 'react'
-import { connect } from 'formik'
-import scrollToElement from 'scroll-to-element'
+import { Component } from "react";
+import { connect } from "formik";
+import scrollToElement from "scroll-to-element";
 
 // Based on this gist: https://gist.github.com/dphrag/4db3b453e02567a0bb52592679554a5b
 class ErrorFocus extends Component {
   static defaultProps = {
     offset: 0,
-    align: 'top',
+    align: "top",
     focusDelay: 200,
-    ease: 'linear',
+    ease: "linear",
     duration: 1000
-  }
+  };
 
-  componentDidUpdate (prevProps) {
-    const { isSubmitting, isValidating, errors } = prevProps.formik
-    const keys = Object.keys(errors)
+  /**
+   *  @errors - object, get errors from formik
+   *  @firstNameValue - String, get first key error from errors object
+   */
+  getFirstElementErrors = (errors, firstNameValue) => {
+    const firstError = errors[firstNameValue];
+
+    if (typeof firstError === "object" && !Array.isArray(firstError)) {
+      const keysLayerOne = Object.keys(firstError);
+      return `${firstElement}-${keysLayerOne[0]}`;
+    }
+
+    if (typeof firstError === "object" && Array.isArray(firstError)) {
+      for (let i = 0; i < firstError.length; i++) {
+        if (firstError[i]) {
+          const elements = Object.keys(firstError[i]);
+          return `${firstNameValue}[${i}].${elements[0]}`;
+        }
+      }
+    }
+    return firstNameValue;
+  };
+
+  componentDidUpdate(prevProps) {
+    const { isSubmitting, isValidating, errors } = prevProps.formik;
+    const keys = Object.keys(errors);
 
     if (keys.length > 0 && isSubmitting && !isValidating) {
-      const selector = `[name="${keys[0]}"]`
-      const errorElement = document.querySelector(selector)
+      const getNameInputError = this.getFirstElementErrors(errors, keys[0]);
+      const selector = `[name="${getNameInputError}"]`;
+      const errorElement = document.querySelector(selector);
 
       if (errorElement) {
-        const { offset, ease, duration, focusDelay, align } = this.props
+        const { offset, ease, duration, focusDelay, align } = this.props;
         scrollToElement(errorElement, {
-          offset, ease, duration, align
-        })
+          offset,
+          ease,
+          duration,
+          align
+        });
 
-        this.timeout = setTimeout(() => errorElement.focus(), duration + focusDelay)
+        this.timeout = setTimeout(
+          () => errorElement.focus(),
+          duration + focusDelay
+        );
       }
     }
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     if (this.timeout) {
-      clearTimeout(this.timeout)
+      clearTimeout(this.timeout);
     }
   }
 
-  render () {
-    return null
+  render() {
+    return null;
   }
 }
 
-export default connect(ErrorFocus)
+export default connect(ErrorFocus);
